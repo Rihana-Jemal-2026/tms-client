afe64efc34b92eacf89c412946063d5659811d88
Rihana <rihanawork38@gmail.com>
2026-08-30 15:40:29 -0700

    Add navbar dynamic login and logout UI actions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal, inject, OnInit } from "@angular/core";
-import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from "@angular/router";
 import { EnrollmentStore } from "./store/enrollment.store";
 import { ThemeService } from "./services/theme.service";
+import { AuthService } from "./services/auth.service";
 
 @Component({
   selector: "app-root",
@@ -14,9 +15,16 @@ export class App implements OnInit {
   protected readonly title = signal("tms-client");
   private store = inject(EnrollmentStore);
   themeService = inject(ThemeService);
+  authService = inject(AuthService);
+  private router = inject(Router);
 
   ngOnInit() {
     this.store.loadEnrollments();
     this.store.listenForLiveUpdates();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(["/login"]);
   }
 }
